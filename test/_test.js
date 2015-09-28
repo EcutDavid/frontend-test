8f89f3ce6c332abcdb740842125d8203aa37dca8
David <Ecutwxguan@163.com>
2015-09-28 20:10:19 +0800

    Slight change test case.

--- test/_test.js
+++ test/_test.js
@@ -3,20 +3,23 @@ import SearchBar from '../scripts/components/SearchBar/SearchBar';
 let TestUtils = React.addons.TestUtils;
 
 describe('SearchBar should work', ()=> {
+  let dom = null;
+
+  beforeEach(() => {
+    dom = TestUtils.renderIntoDocument(<SearchBar filters={['Region', 'Last Inpot']}/>);
+  });
+
   it('Test case should work', function() {
-    let dom = TestUtils.renderIntoDocument(<SearchBar />);
     let resDom = TestUtils.scryRenderedDOMComponentsWithClass(dom, 'SearchBar');
     expect(resDom.length).toBe(1);
   });
 
   it('Should have button for drop menu', function() {
-    let dom = TestUtils.renderIntoDocument(<SearchBar />);
     let dropListBtns = TestUtils.scryRenderedDOMComponentsWithClass(dom, 'dropMenuIcon');
     expect(dropListBtns[0] != undefined ).toBe(true);
   });
 
   it('Should display dropMenu', function() {
-    let dom = TestUtils.renderIntoDocument(<SearchBar filters={['Region', 'Last Inpot']}/>);
     let dropListBtn = TestUtils.scryRenderedDOMComponentsWithClass(dom, 'dropMenuIcon')[0];
     let dropItems = TestUtils.scryRenderedDOMComponentsWithClass(dom, 'dropItemArea');
     expect(dropItems.length === 0).toBe(true);
@@ -27,7 +30,6 @@ describe('SearchBar should work', ()=> {
   });
 
   it('Should hide dropMenu after item selected', function() {
-    let dom = TestUtils.renderIntoDocument(<SearchBar filters={['Region', 'Last Inpot']}/>);
     let dropListBtn = TestUtils.scryRenderedDOMComponentsWithClass(dom, 'dropMenuIcon')[0];
 
     TestUtils.Simulate.click(dropListBtn);
